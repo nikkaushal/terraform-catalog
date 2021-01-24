933d90f3ad2cc34ce50d48037dffd5d3e24e1efd
nikkaushal <nikkaushal@gmail>
2021-01-24 16:11:41 -0500

    catalogue

--- asg.tf
+++ asg.tf
@@ -10,15 +10,23 @@ module "asg" {
 }
 
 resource "aws_lb_listener_rule" "catalogue" {
-  listener_arn = data.terraform_remote_state.alb.outputs.PRIVATE_ALB_LISTENER_ARN
-  action {
-    type = "forward"
+  listener_arn        = data.terraform_remote_state.alb.outputs.PRIVATE_ALB_LISTENER_ARN
+  action              {
+    type             = "forward"
     target_group_arn = module.asg.TG_ARN
   }
-  condition {
-   host_header {
-     values = ["catalogue-${var.ENV}.devopsnik.tk"]
+  condition            {
+   host_header        {
+     values         = ["catalogue-${var.ENV}.devopsnik.tk"]
    }
   }
 
+}
+
+resource "aws_route53_record" "alb" {
+  zone_id = "Z0869909I3INDIN3CQDX"
+  name    = "${var.COMPONENT}-${var.ENV}.devopsnik.tk"
+  type    = "CNAME"
+  ttl     = "300"
+  records = [data.terraform_remote_state.alb.outputs.PRIVATE_ALB_DNS]
 }
